72f068bb34447c7a90e27fc2c47edbf56352d46e
khe004 <njuhekai@gmail.com>
2018-01-19 17:58:58 -0800

    update 002.add-two-numbers.python3.py

---
+++ 002.add-two-numbers.python3.py
@@ -0,0 +1,57 @@
+#
+# [2] Add Two Numbers
+#
+# https://leetcode.com/problems/add-two-numbers/description/
+#
+# algorithms
+# Medium (28.33%)
+# Total Accepted:    422.4K
+# Total Submissions: 1.5M
+# Testcase Example:  '[2,4,3]\n[5,6,4]'
+#
+# You are given two non-empty linked lists representing two non-negative
+# integers. The digits are stored in reverse order and each of their nodes
+# contain a single digit. Add the two numbers and return it as a linked list.
+# 
+# You may assume the two numbers do not contain any leading zero, except the
+# number 0 itself.
+# 
+# 
+# Example
+# 
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# Explanation: 342 + 465 = 807.
+# 
+# 
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        dummy = ListNode(0)
+        head = dummy
+        while l1 or l2 or carry:
+            val = carry
+            if l1:
+                val += l1.val
+                l1 = l1.next
+            if l2:
+                val += l2.val
+                l2 = l2.next
+            carry = val // 10
+            val = val % 10
+            head.next = ListNode(val)
+            head = head.next
+
+        return dummy.next
